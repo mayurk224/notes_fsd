e66c303a4b24e8f275aeac07fc2b07609685c4ac
mayurk224 <mayurkamble0250@gmail.com>
2026-02-05 18:55:08 +0530

    fix: update script and stylesheet paths in index.html; modify static file serving in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static("./public"));
 
 app.use(cors())
 
